913a1eccc790a04959379cac5de05116f2c5546c
Usame Algan <usame.algan@gnosis.pm>
2022-04-11 17:35:43 +0200

    feat: Add Overview widget for dashboard

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -7,12 +7,13 @@ import Row from 'src/components/layout/Row'
 import Col from 'src/components/layout/Col'
 import PendingTxsList from 'src/components/Dashboard/PendingTxs/PendingTxsList'
 
-import AddSafeWidget from 'src/components/Dashboard/AddSafe'
 import CreateSafeWidget from 'src/components/Dashboard/CreateSafe'
+import Overview from 'src/components/Dashboard/Overview/Overview'
+import { lg } from 'src/theme/variables'
 
 const Card = styled.div`
   background: #fff;
-  padding: 40px;
+  padding: ${lg};
   border-radius: 8px;
   flex: 1;
   margin: 10px;
@@ -37,7 +38,7 @@ function Home(): ReactElement {
 
       <Row>
         <Card>
-          <AddSafeWidget />
+          <Overview />
         </Card>
 
         <Card>
